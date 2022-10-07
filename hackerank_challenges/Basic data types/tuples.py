affc913436940e74bdb0d9a4dff3bffd695908ba
qristinius <qristina.qristina20010203@gmail.com>
2022-10-07 23:07:24 +0400

    new

---
+++ hackerank_challenges/Basic data types/tuples.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    integer_list = tuple(map(int, input().split()))
+    print(hash(integer_list))
+
+
+#hackerrankshi mushaobs tu pypy3zea da ara python3 ze 
