0f4cd2f29460f40b9c616360dc2c2612a040f264
qristinius <qristina.qristina20010203@gmail.com>
2022-11-28 01:56:13 +0400

    mathup

---
+++ hackerank_challenges/Math/Integers_come_in_all_sizes.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math as m 
+a,b,c,d = [int(input()) for i in "1234"]
+
+
+print(pow(a,b) + pow(c,d))
---
+++ hackerank_challenges/Math/Power_Mod_power.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math as m 
+
+a = input()
+b = input()
+c = input()
+
+print(pow(int(a),int(b)))
+
+print(pow(int(a),int(b), int(c)))
