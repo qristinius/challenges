b7da7e0b052b9cbdcec9621ab6fdeeae8352d171
Kristine Dzneladze <qristina.qristina20010203@gmail.com>
2023-08-21 17:43:33 +0400

    easy upd

---
+++ hackerank_challenges/SQL/Easy/Weather_observation_station_13.sql
@@ -0,0 +1,2 @@
+select format (sum(lat_n), "0.0000") from station 
+where lat_n between 38.7880 and 137.2345 ;
---
+++ hackerank_challenges/SQL/Easy/Weather_observation_station_14.sql
@@ -0,0 +1,2 @@
+select format(max(lat_n), "0.0000") from station 
+where lat_n < 137.2345; 
