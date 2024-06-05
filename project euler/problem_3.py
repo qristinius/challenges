481389868b39bc3d12e0c6a6a158a3824ca3582a
kristinedzneladze <kristine.dzneladze.1@iliauni.edu.ge>
2024-06-05 15:43:52 +0400

    new challenges added

--- project euler/problem_3.py
+++ project euler/problem_3.py
@@ -1,17 +1,22 @@
-#euler project
-#project N3 
+# the number that I want the prime factors of
+num = 600851475143
 
-num = 600851475143  #the number that i want the prime factors of 
-factors_of_num = [] #empty list that i want to add prime factors 
+# empty list that I want to add prime factors
+factors_of_num = []
 
-#logics for adding info in lits
+#logics for adding info in list
 while True:
-    for i in range (2,int(num)+1):  #the action happens in range of 2 and the presented number 
+    # the action happens in range of 2 and the presented number
+    for i in range (2,int(num)+1):
         if num % i == 0 :
-            factors_of_num.append(i) #appends the list the number that is factor of the presented number
-            num = num/i              #divide the previous number on it's factor and rewrite it in "num" variable   
-            break #breaks the for cycle and restarts for new divided number form first i to up 
-    if num == 1: 
-        break    #when the lastly divided numbers is equal to 1 the while loop cycle breaks 
+            # appends the list the number that is factor of the presented number
+            factors_of_num.append(i)
+            # divide the previous number on it's factor and rewrite it in "num" variable
+            num = num/i
+            # breaks the for cycle and restarts for new divided number form first i to up
+            break
+    if num == 1:
+        # when the lastly divided numbers is equal to 1 the while loop cycle breaks
+        break
 
 print(max(factors_of_num))
